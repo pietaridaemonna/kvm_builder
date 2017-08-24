487e986a9616f96171b84392655dbe9a98988563
peter ducai <peter.ducai@telekom.com>
2017-08-24 13:47:47 +0200

    ubuntu snaps and iso

--- templates/download_images.sh
+++ templates/download_images.sh
@@ -12,10 +12,14 @@ sudo wget https://download.fedoraproject.org/pub/fedora/linux/releases/26/Server
 # FEDORA 26 ATOMIC
 sudo wget https://download.fedoraproject.org/pub/alt/atomic/stable/Fedora-Atomic-26-20170807.0/Atomic/x86_64/iso/Fedora-Atomic-ostree-x86_64-26-20170807.0.iso -P /var/lib/libvirt/images/
 
-# UBUNTU
+# UBUNTU 16.04 SERVER
+sudo wget https://www.ubuntu.com/download/server/thank-you?version=16.04.3&architecture=amd64 -P /var/lib/libvirt/images/
 
+# UBUNTU SNAPPY
+sudo wget http://cdimage.ubuntu.com/ubuntu-core/16/stable/current/ubuntu-core-16-amd64.img.xz -P /var/lib/libvirt/images/
 
+# CORE OS 1465.6.0 STABLE
+sudo wget https://stable.release.core-os.net/amd64-usr/current/coreos_production_iso_image.iso -P /var/lib/libvirt/images/
 # DEBIAN
-
 
 # ALPINE?
